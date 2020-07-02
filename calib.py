36426d842b064fe12d7727348dfba181138fadbc
barryyan0121 <yanhan0121@gmail.com>
2020-07-01 22:54:58 -0400

    update source code

--- calib.py
+++ calib.py
@@ -11,6 +11,7 @@ objp[:,:2] = np.mgrid[0:8, 0:5].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d points in real world space
 imgpoints = [] # 2d points in image plane.
+subpix_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.01)
 
 # Make a list of calibration images
 images = glob.glob('/home/barry/Desktop/Camera_Calibration/pic/IR_camera_calib_img/*.png')
@@ -25,6 +26,8 @@ for idx, fname in enumerate(images):
 
     # If found, add object points, image points
     if ret == True:
+        # Refining corners position with sub-pixels based algorithm
+        cv2.cornerSubPix(gray, corners, (3, 3), (-1, -1), subpix_criteria)
         objpoints.append(objp)
         imgpoints.append(corners)
 
@@ -36,9 +39,6 @@ for idx, fname in enumerate(images):
 
 cv2.destroyAllWindows()
 
-import pickle
-
-
 # Test undistortion on an image
 img = cv2.imread('/home/barry/Desktop/Camera_Calibration/pic/img/calib.png')
 img_size = (img.shape[1], img.shape[0])
@@ -46,6 +46,20 @@ img_size = (img.shape[1], img.shape[0])
 # Do camera calibration given object points and image points
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
 
+print (("ret:"),ret)
+print (("internal matrix:\n"),mtx)
+# in the form of (k_1,k_2,p_1,p_2,k_3)
+print (("distortion cofficients:\n"),dist)
+print (("rotation vectors:\n"),rvecs)
+print (("translation vectors:\n"),tvecs)
+
+# calculate the error of reproject
+total_error = 0
+for i in range(len(objpoints)):
+    img_points_repro, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv2.norm(imgpoints[i], img_points_repro, cv2.NORM_L2)/len(img_points_repro)
+    total_error += error
+print(("Average Error of Reproject: "), total_error/len(objpoints))
 
 dst = cv2.undistort(img, mtx, dist, None, mtx)
 cv2.imwrite('/home/barry/Desktop/Camera_Calibration/pic/save_dedistortion/calibrated_img.png',dst)
